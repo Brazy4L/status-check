0dde29f58e4bd85eef328aad00b7f048e19774c1
Brazy <me@brazy.one>
2023-03-11 20:05:29 +0300

    Add new item

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,5 @@
 export const data = [
   { id: 0, url: "https://google.com", status: 'pending' },
   { id: 1, url: "https://play.google.com", status: 'pending' },
+  { id: 2, url: "https://twitter.com", status: 'pending' },
 ];
